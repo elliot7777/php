825f0df045c95a7970aa3f7439a1ede9ab33f710
elliot7777 <74612269+elliot7777@users.noreply.github.com>
2021-07-23 23:34:16 -0400

    Add files via upload

---
+++ hack.php
@@ -0,0 +1,22 @@
+<html>
+<head><title>H / A / C / K</title>
+<meta charset="utf-8"><!-- this code prevent cross site scriptingb injection -->
+<link rel="stylesheet" href="stl.css">
+</head>
+<body>
+<h1><?php echo 'This message is from serv'; ?></h1>
+<center style="font-size:70;">cyber security<center>
+<form>
+<center>
+<input type = "password" placeholder = "password">
+<br>
+<input id="a" type = "email" placeholder ="email">
+<br>
+
+<script>
+
+</script>
+<button> login</button>
+</form><center/>
+</body>
+</html>
